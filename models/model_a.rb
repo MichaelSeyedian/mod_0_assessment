7651a82a304f7edd22c694857db4463b58947347
Michael Seyedian <seyedian.mike@gmail.com>
2021-12-16 19:23:53 -0700

    Add dinosaur model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  species: "Tyrannosaurus rex",
+  nickname: "T-Rex",
+  speed_in_mph: 17,
+  lifespan_in_years: 30,
+  herbivore: false,
+  carnivore: true,
+  signature_features: ["huge teeth", "short arms", "three-toed feet"],
+  moview_depicted: ["Jurassic Park", "The Land Before Time", "Night at the Museum"]
+}
