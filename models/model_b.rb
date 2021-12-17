24dec9be3e47e9cbd98301e0e792809c6fd31420
Michael Seyedian <seyedian.mike@gmail.com>
2021-12-16 19:33:22 -0700

    Add developer model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  name: "Michael",
+  focus: "Back-End",
+  years_of_experience: 0,
+  desired_salary: 1000000.00,
+  motivated: true,
+  work_from_home: true,
+  programming_languages: ["Ruby", "SQL"],
+  desired_employers: ["Twitter", "Hotel Engine", "GitHub"]
+}
