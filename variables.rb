09f9b9e3fa2742c7ec2405e498a5d04da7d84f42
Michael Seyedian <seyedian.mike@gmail.com>
2021-12-16 19:07:02 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,10 @@
+movie = "The Life Aquatic"
+is_entertaining = true
+movie = "Jaws"
+num_4 = 4
+num_6 = 6
+product = num_6 * num_4
+favorite_foods = ["pizza", "cereal", "mashed potatoes"]
+
+favorite_foods.shift()
+favorite_foods.length
